08723cd75724a93119709c5ffca15ecf03082846
Ihor Vysotskyi <lgorvysocky@gmail.com>
2021-08-01 20:04:29 +0200

    remove

--- Assets/Scripts/FlyingSquareComponentAction.cs
+++ Assets/Scripts/FlyingSquareComponentAction.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class FlyingSquareComponentAction : MonoBehaviour
 {
-    [SerializeField] private Light _light;
     [SerializeField] private float _moveSpeed = 10f;
 
     private void Awake()
@@ -16,7 +15,6 @@ public class FlyingSquareComponentAction : MonoBehaviour
         action(() =>
         {
             DebugLogUtil.log(GetType(), "BLOCKER-LOG: --- Start() --- : " + Time.deltaTime);
-            _light = GetComponent<Light>();
             DebugLogUtil.log(GetType(), "BLOCKER-LOG: position.x -> " + transform.position.x);
             DebugLogUtil.log(GetType(), "BLOCKER-LOG: activeSelf -> " + gameObject.activeSelf);
             DebugLogUtil.log(GetType(), "BLOCKER-LOG: activeInHierarchy -> " + gameObject.activeInHierarchy);
@@ -36,14 +34,17 @@ public class FlyingSquareComponentAction : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.B))
                 GetComponent<Renderer>().material.color = Color.blue;
             
-            if (Input.GetKeyUp(KeyCode.Space))
-                _light.enabled = !_light.enabled;
-            
             if(Input.GetKey(KeyCode.LeftArrow))
                 transform.Translate(Vector3.left * (_moveSpeed * Time.deltaTime));
         
             if(Input.GetKey(KeyCode.RightArrow))
-                transform.Translate(Vector3.right * (_moveSpeed * Time.deltaTime));        });
+                transform.Translate(Vector3.right * (_moveSpeed * Time.deltaTime));
+
+            if (transform.position.y < -4f)
+            {
+                Destroy(gameObject);
+            }
+        });
     }
 
     private void action(ActionInterface actionInterface) {
